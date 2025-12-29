5d18755b328fa64415c255bc441ad2782e1b309b
UmairAhmad5453 <umair5453@gmail.com>
2025-12-29 15:20:29 +0500

    Remove compiled view files from git tracking

--- storage/framework/views/e65a6dec36be5be1bdc7ad83b57bbd5b.php
+++
@@ -1,8 +0,0 @@
-<?php
-$containerFooter = (isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact') ? 'container-xxl' : 'container-fluid';
-?>
-
-<!-- Footer-->
-
-<!--/ Footer-->
-<?php /**PATH C:\xampp\htdocs\erpbk\resources\views/layouts/sections/footer/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/f3a3d5de126b06f190614b4075bd5070.php
+++
@@ -1,8 +0,0 @@
-<?php
-$containerFooter = (isset($configData['contentLayout']) && $configData['contentLayout'] === 'compact') ? 'container-xxl' : 'container-fluid';
-?>
-
-<!-- Footer-->
-
-<!--/ Footer-->
-<?php /**PATH /var/www/laravel/resources/views/layouts/sections/footer/footer.blade.php ENDPATH**/ ?>

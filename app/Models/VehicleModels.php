f9918df75c9e4976afe6ea5a9e8c0903c546bf3c
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-04-08 19:44:59 +0500

    Add new changes for laralve Cloud

--- app/Models/VehicleModels.php
+++ app/Models/VehicleModels.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Traits\LogsActivity;
 
 class VehicleModels extends Model
 {
     use HasFactory, LogsActivity;
+
     protected $fillable = [
         'name',
-        'status'
+        'status',
     ];
 }

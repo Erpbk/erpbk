04d803c4debcb68ad2a017107afa777dd2de28a3
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-04-25 15:24:56 +0500

    Update action-buttons.blade.php

--- resources/views/riders/action-buttons.blade.php
+++ resources/views/riders/action-buttons.blade.php
@@ -1,13 +1,16 @@
 <!-- Action Buttons Component -->
 @php
 $companySlug = request()->route('company_slug');
+$isRiderEditPage = request()->routeIs('riders.edit');
 @endphp
+@if(!$isRiderEditPage)
 <div class="card-footer bg-light border-top fixed-footer mt-3">
     <div class="d-flex justify-content-start gap-2 flex-wrap">
-        @isset($result)
+        @can('rider_edit')
         <a href="{{ route('riders.edit', ['company_slug' => $companySlug, 'rider' => $result['id']]) }}" class="btn btn-outline-primary btn-sm waves-effect waves-light">
             <i class="fa fa-edit"></i>&nbsp;Edit
         </a>
+        @endcan
         @can('email_create')
         <a href="javascript:void(0);" data-action="{{ route('rider.sendemail', ['company_slug' => $companySlug, 'id' => $result['id']]) }}" data-size="md"
             data-title="{{ $result['name'] . ' (' . $result['rider_id'] . ')' }}"
@@ -20,6 +23,6 @@ $companySlug = request()->route('company_slug');
             <i class="fas fa-chart-bar"></i>&nbsp;Add Timeline
         </a>
         @endcan
-        @endisset
     </div>
 </div>
+@endif

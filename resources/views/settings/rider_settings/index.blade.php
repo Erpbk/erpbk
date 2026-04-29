43369605a0b3ab33175debb9d5d17fb2be25d1fa
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-04-29 19:04:09 +0500

    Update index.blade.php

--- resources/views/settings/rider_settings/index.blade.php
+++ resources/views/settings/rider_settings/index.blade.php
@@ -1232,9 +1232,7 @@
   (function() {
     'use strict';
 
-    const dataTypesMeta = {
-      !!json_encode($dataTypes, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT) !!
-    };
+    const dataTypesMeta = JSON.parse((document.getElementById('riderDataTypesMetaJson') && document.getElementById('riderDataTypesMetaJson').value) || '{}');
 
     function buildConfigFields(container, typeKey, existingConfig) {
       container.innerHTML = '';

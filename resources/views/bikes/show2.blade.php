7de852d57483b96ee5d633f02a704eb9cd0a9098
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-05-04 15:54:09 +0500

    Update show2.blade.php

--- resources/views/bikes/show2.blade.php
+++ resources/views/bikes/show2.blade.php
@@ -8,7 +8,7 @@ $fieldsByCategory = $fieldsByCategory ?? \App\Models\BikeCustomField::fieldsByCa
 
 <div class="card mb-4">
   <div class="card-header bg-primary text-white d-flex align-items-center justify-content-between flex-wrap gap-2">
-    <h5 class="mb-0">Bike Fields</h5>
+    <h5 class="mb-0 text-white">Bike Fields</h5>
   </div>
   <div class="card-body">
     <div class="row">

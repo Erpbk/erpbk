a17c9cc0e7eea17d7649ff6067198b3e998c34d8
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-05-04 18:51:32 +0500

    Update fields.blade.php

--- resources/views/supplier_invoices/fields.blade.php
+++ resources/views/supplier_invoices/fields.blade.php
@@ -38,7 +38,15 @@ $garages = \App\Models\Garages::where('status',1)->where('garage_type' , 'intern
     </div>
     <div class="col-md-2 form-group">
         <label>Garage</label>
-        {!! Form::select('garage_id', $garages, isset($invoice) ? $invoice->garage_id : null, ['class' => 'form-select form-select-sm select2', 'required' => true]) !!}
+        <select class="form-select" id="garage_id" name="garage_id" required>
+            <option value="">Select Garage</option>
+            @foreach($garages as $garage)
+            <option value="{{ $garage->id }}"
+                {{ isset($invoice) && $invoice->garage_id == $garage->id ? 'selected' : '' }}>
+                {{ $garage->name }}
+            </option>
+            @endforeach
+        </select>
     </div>
     @else
     <div class="col-md-2 form-group">
@@ -194,6 +202,10 @@ $garages = \App\Models\Garages::where('status',1)->where('garage_type' , 'intern
             allowClear: true
         });
         $('.item').select2({
+            dropdownParent: $('#modalTopbody'),
+            allowClear: true
+        });
+        $('#garage_id').select2({
             dropdownParent: $('#modalTopbody'),
             allowClear: true
         });

425c432caa6f3e72e5bdacbf101927e1ff556df8
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-05-04 15:52:55 +0500

    update teh bike infor

--- resources/views/bikes/show2.blade.php
+++ resources/views/bikes/show2.blade.php
@@ -2,33 +2,13 @@
 
 @section('page_content')
 @php
-  // Show configured bike fixed/custom fields (grouped by category).
-  $fieldsByCategory = $fieldsByCategory ?? \App\Models\BikeCustomField::fieldsByCategoryForForm();
+// Show configured bike fixed/custom fields (grouped by category).
+$fieldsByCategory = $fieldsByCategory ?? \App\Models\BikeCustomField::fieldsByCategoryForForm();
 @endphp
 
 <div class="card mb-4">
   <div class="card-header bg-primary text-white d-flex align-items-center justify-content-between flex-wrap gap-2">
     <h5 class="mb-0">Bike Fields</h5>
-    <div class="d-flex align-items-center gap-2 ms-auto">
-      @php
-        $mulkiyaAuthorized = auth()->user()->can('bike_document');
-      @endphp
-      @if($mulkiyaFile)
-        <a href="{{ url('storage2/' . $mulkiyaFile->type . '/' . $mulkiyaFile->type_id . '/' . $mulkiyaFile->file_name) }}" target="_blank" class="btn btn-light btn-sm">
-          <i class="ti ti-download"></i> Mulkiya
-        </a>
-      @elseif($mulkiyaAuthorized)
-        <a class="btn btn-light btn-sm show-modal action-btn"
-          href="javascript:void(0);"
-          data-action="{{ route('files.create', ['type_id' => $bikes->id, 'type' => 'bike', 'suggested_name' => 'Mulkiya']) }}"
-          data-size="sm"
-          data-title="Upload File">
-          <i class="ti ti-upload"></i> Upload Mulkiya
-        </a>
-      @else
-        <span class="small text-white-50">No Mulkiya</span>
-      @endif
-    </div>
   </div>
   <div class="card-body">
     <div class="row">
--- resources/views/bikes/show_fields_by_category.blade.php
+++ resources/views/bikes/show_fields_by_category.blade.php
@@ -1,17 +1,36 @@
 @foreach($fieldsByCategory as $group)
-    <div class="col-12">
-        <div class="card border mb-4">
-            <div class="card-header">
-                <b>{{ $group->category->label }}</b>
+<div class="col-12">
+    <div class="card border mb-4">
+        <div class="card-header">
+            <b>{{ $group->category->label }}</b>
+            <div class="d-flex align-items-center gap-2 ms-auto">
+                @php
+                $mulkiyaAuthorized = auth()->user()->can('bike_document');
+                @endphp
+                @if($mulkiyaFile)
+                <a href="{{ url('storage2/' . $mulkiyaFile->type . '/' . $mulkiyaFile->type_id . '/' . $mulkiyaFile->file_name) }}" target="_blank" class="btn btn-light btn-sm">
+                    <i class="ti ti-download"></i> Mulkiya
+                </a>
+                @elseif($mulkiyaAuthorized)
+                <a class="btn btn-light btn-sm show-modal action-btn"
+                    href="javascript:void(0);"
+                    data-action="{{ route('files.create', ['type_id' => $bikes->id, 'type' => 'bike', 'suggested_name' => 'Mulkiya']) }}"
+                    data-size="sm"
+                    data-title="Upload File">
+                    <i class="ti ti-upload"></i> Upload Mulkiya
+                </a>
+                @else
+                <span class="small text-white-50">No Mulkiya</span>
+                @endif
             </div>
-            <div class="card-body">
-                <div class="row">
-                    @foreach($group->fields as $item)
-                        @include('bikes._show_field', ['item' => $item])
-                    @endforeach
-                </div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                @foreach($group->fields as $item)
+                @include('bikes._show_field', ['item' => $item])
+                @endforeach
             </div>
         </div>
     </div>
+</div>
 @endforeach
-

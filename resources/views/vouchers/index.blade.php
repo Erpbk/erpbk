034669a3bd37f374365fbb2e8a71f37d23ea977d
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-05-19 18:54:42 +0500

    Update index.blade.php

--- resources/views/vouchers/index.blade.php
+++ resources/views/vouchers/index.blade.php
@@ -95,15 +95,15 @@
                         href="javascript:void(0);" data-size="sm" data-title="Import Voucher" data-action="{{ route('voucher.import') }}">
                         Import Voucher
                     </a>
-                    @foreach($voucherTypesForFilter ?? [] as $key => $value)
+                    @if(isset(($voucherTypesForFilter ?? [])['JV']))
                     <a class="show-modal action-btn btn btn-primary" style="margin-right:5px;"
                         href="javascript:void(0);"
                         data-size="xl"
-                        data-title="Create {{ $value }}"
-                        data-action="{{ route('vouchers.create', ['vt' => $key]) }}">
-                        <i class="fa fa-plus"></i>&nbsp;{{ $value }}
+                        data-title="Create {{ $voucherTypesForFilter['JV'] }}"
+                        data-action="{{ route('vouchers.create', ['vt' => 'JV']) }}">
+                        <i class="fa fa-plus"></i>&nbsp;{{ $voucherTypesForFilter['JV'] }}
                     </a>
-                    @endforeach
+                    @endif
                     @endcan
                 </div>
             </div>

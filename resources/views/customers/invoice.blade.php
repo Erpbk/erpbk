c392f066ea0e78fbdf72fbd4877b325471e7923a
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-05-21 17:08:13 +0500

    update the custome invoice print option

--- resources/views/customers/invoice.blade.php
+++ resources/views/customers/invoice.blade.php
@@ -5,26 +5,26 @@
     }
 </style>
 @section('page_content')
-    <div class="content">
-        @include('flash::message')
-        <div class="clearfix"></div>
-        @can('receipt_view')
-        <div class="card">
-            <div class="card-header d-flex justify-content-between">
-                <div class="card-search">
-                    <input type="text" id="quickSearch" name="quick_search" class="form-control" placeholder="Quick Search..." value="{{ request('quick_search') }}">
-                </div>
-                @can('customer_invoice_create')
-                    <button class="btn btn-primary btn-sm show-modal" href="javascript:void(0);" data-size="xl" data-title="Add New Receipt" data-action="{{ route('customer_invoices.create') }}?customer_id={{ $customer->id }}">Add New</button>
-                @endcan
+<div class="content">
+    @include('flash::message')
+    <div class="clearfix"></div>
+    @can('receipt_view')
+    <div class="card">
+        <div class="card-header d-flex justify-content-between">
+            <div class="card-search">
+                <input type="text" id="quickSearch" name="quick_search" class="form-control" placeholder="Quick Search..." value="{{ request('quick_search') }}">
             </div>
-            @include('customer_invoices.table')
+            @can('customer_invoice_create')
+            <button class="btn btn-primary btn-sm show-modal" href="javascript:void(0);" data-size="xl" data-title="Add New Receipt" data-action="{{ route('customer_invoices.create') }}?customer_id={{ $customer->id }}">Add New</button>
+            @endcan
         </div>
-        @endcan
-        @cannot('receipt_view')
-            <div class="text-center mt-5">
-                <h3>You do not have permission to view Receipts.</h3> 
-            </div>
-        @endcannot
+        @include('customer_invoices.table')
     </div>
+    @endcan
+    @cannot('receipt_view')
+    <div class="text-center mt-5">
+        <h3>You do not have permission to view Receipts.</h3>
+    </div>
+    @endcannot
+</div>
 @endsection

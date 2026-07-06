a1de5366f3b07427a46ef9b15c1cce8ba11adb26
Malik Abubakar <96823154+MalikAbubakar890@users.noreply.github.com>
2026-07-06 22:21:49 +0500

    Update _status_badges.blade.php

--- resources/views/riders/_status_badges.blade.php
+++ resources/views/riders/_status_badges.blade.php
@@ -1,10 +1,18 @@
 @php
 $employment = $employment ?? \App\Models\Riders::employmentStatusDisplay($employmentStatus ?? null);
 $option = $option ?? \App\Models\Riders::riderOptionStatusBadge($optionText ?? null);
+$statusDays = $statusDays ?? null;
+$statusChangedAt = $statusChangedAt ?? null;
+$daysTitle = $statusChangedAt
+  ? 'Status changed on ' . \Carbon\Carbon::parse($statusChangedAt)->format('d M Y')
+  : 'Days in current status';
 @endphp
 <div class="d-flex flex-wrap align-items-center gap-1 justify-content-center">
   <span class="badge {{ $employment['badge'] }}" title="Employment / assignment status">{{ $employment['label'] }}</span>
   @if($option)
   <span class="badge {{ $option['badge'] }}" title="Rider option / flag">{{ $option['label'] }}</span>
+  @endif
+  @if($statusDays !== null && $statusDays !== '')
+  <span class="badge bg-label-secondary" title="{{ $daysTitle }}">{{ (int) $statusDays }} {{ (int) $statusDays === 1 ? 'day' : 'days' }}</span>
   @endif
 </div>
